0b34094c82c07cef0046905cc8d2e1b68ec97378
Branimir Vujicic <branimir@axiomq.com>
2023-09-12 12:33:20 +0200

    Add Feature Toggles
    Feature Toggles should allow teams to modify system behavior without changing
    code. Feature Toggles are configured using google guice. Basic definition of
    toggles are crated using FeatureToggleBinder. FeatureToggleBinder creates
    FeatureToggle and additional configuration can be done using feature
    configuration.
    In current stage Feature Toggles supports:
    - if / else based feature toggles
    - Dependency Injection based
      Hot reloading implementation without restart require code refactoring to
      add an interface when injecting the new implementation/class
    - using various toggle strategies along with simple on / off toggles
    
    configuration:
    
    to allow feature toggle configuration four lines are needed in
    config.properties file
    ```
    features.config-source-type=file
    features.config-source=/etc/feature-config.properties
    features.config-type=properties
    features.refresh-period=30s
    ```
    
    `configuration-source-type` is source type for Feature Toggles configuration
    `features.config-source` is a source (file) of the configuration
    `features.config-type` format in which configuration is stored (json or properties)
    `features.refresh-period` configuration refresh period
    
    Defining Feature Toggles
    
    Feature toggle definition is done in google guice module using `FeatureToggleBinder`
    
    simple feature toggle definition
    ```
        featureToggleBinder(binder)
                            .featureId("featureXX")
                            .bind()
    ```
    This example creates bindings for @Inject
    ```
        @Inject
        public Runner(@FeatureToggle("featureXX") Supplier<Boolean> isFeatureXXEnabled)
        {
            this.isFeatureXXEnabled = isFeatureXXEnabled;
        }
    ```
    `isFeatureXXEnabled` can be used to test if feature is enabled or disabled:
    ```
        boolean testFeatureXXEnabled()
        {
            return isFeatureXXEnabled.get();
        }
    ```
    
    hot reloadable feature toggle definition
    ```
    featureToggleBinder(binder, Feature01.class)
                            .featureId("feature01")
                            .baseClass(Feature01.class)
                            .defaultClass(Feature01Impl01.class)
                            .allOf(Feature01Impl01.class, Feature01Impl02.class)
                            .bind()
    ```
    adding Feature Toggle switching strategy
    ```
    featureToggleBinder(binder)
                            .featureId("feature04")
                            .toggleStrategy("AllowAll")
                            .toggleStrategyConfig(ImmutableMap.of("key", "value", "key2", "value2"))
    ```
    
    feature-config.properties file example
    ```
    # feature query-logger
    feature.query-logger.enabled=true
    feature.query-logger.strategy=OsToggle
    feature.query-logger.strategy.os_name=.*Linux.*
    
    #feature.query-rate-limiter
    feature.query-rate-limiter.currentInstance=com.facebook.presto.server.protocol.QueryBlockingRateLimiter
    
    # feature.query-cancel
    feature.query-cancel.strategy=AllowList
    feature.query-cancel.strategy.allow-list-source=.*IDEA.*
    feature.query-cancel.strategy.allow-list-user=.*prestodb
    ```
    in this example for first feature `query-logger`
    changing value of feature.query-logger.enabled to `false` will 'disable' this feature.
    Changes will be effective within refresh period.
    Pass column delimiter info to reader (#6338)
    
    Summary: Pull Request resolved: https://github.com/facebookincubator/velox/pull/6338
    
    Reviewed By: Yuhta
    
    Differential Revision: D48457913
    
    fbshipit-source-id: 57d76dfa229de3801bf3181f780a485b628427ad

---
+++ presto-feature-toggle/src/main/java/com/facebook/presto/features/binder/PrestoFeatureToggle.java
@@ -0,0 +1,163 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.features.binder;
+
+import com.facebook.presto.features.annotations.FeatureToggle;
+import com.facebook.presto.features.strategy.FeatureToggleStrategy;
+import com.facebook.presto.features.strategy.FeatureToggleStrategyFactory;
+import com.facebook.presto.spi.features.FeatureConfiguration;
+import com.facebook.presto.spi.features.FeatureToggleConfiguration;
+import com.facebook.presto.spi.features.FeatureToggleStrategyConfig;
+import com.google.inject.Inject;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.Objects.requireNonNull;
+
+public class PrestoFeatureToggle
+{
+    private final Map<String, Feature<?>> featureMap;
+    private final Map<String, Object> featureInstanceMap;
+    private final FeatureToggleConfiguration featureToggleConfiguration;
+    private final FeatureToggleStrategyFactory featureToggleStrategyFactory;
+
+    @Inject
+    public PrestoFeatureToggle(
+            @FeatureToggle("feature-map") Map<String, Feature<?>> featureMap,
+            @FeatureToggle("feature-instance-map") Map<String, Object> featureInstanceMap,
+            FeatureToggleConfiguration featureToggleConfiguration,
+            FeatureToggleStrategyFactory featureToggleStrategyFactory)
+    {
+        this.featureMap = requireNonNull(featureMap);
+        this.featureInstanceMap = requireNonNull(featureInstanceMap);
+        this.featureToggleConfiguration = requireNonNull(featureToggleConfiguration);
+        this.featureMap.values().forEach(f -> f.setContext(this));
+        this.featureToggleStrategyFactory = requireNonNull(featureToggleStrategyFactory);
+    }
+
+    public boolean isEnabled(String featureId)
+    {
+        AtomicBoolean enabled = new AtomicBoolean(true);
+        checkEnabledFromStaticConfiguration(featureId, enabled);
+        checkEnabledFromDynamicConfiguration(featureId, enabled);
+        checkEnabledFromToggleStrategy(featureId, enabled);
+        return enabled.get();
+    }
+
+    public boolean isEnabled(String featureId, Object object)
+    {
+        AtomicBoolean enabled = new AtomicBoolean(true);
+        checkEnabledFromStaticConfiguration(featureId, enabled);
+        // dynamic configuration overrides static configuration
+        checkEnabledFromDynamicConfiguration(featureId, enabled);
+        checkEnabledFromToggleStrategy(featureId, object, enabled);
+        return enabled.get();
+    }
+
+    private void checkEnabledFromToggleStrategy(String featureId, AtomicBoolean enabled)
+    {
+        if (enabled.get()) {
+            FeatureConfiguration dynamicFeatureConfiguration = featureToggleConfiguration.getFeatureConfiguration(featureId);
+            FeatureConfiguration staticFeatureConfiguration = featureMap.get(featureId).getConfiguration();
+            FeatureConfiguration featureConfiguration = null;
+            String toggleStrategyClass = null;
+            if (dynamicFeatureConfiguration != null && dynamicFeatureConfiguration.getFeatureToggleStrategyConfig().isPresent()) {
+                toggleStrategyClass = dynamicFeatureConfiguration.getFeatureToggleStrategyConfig().get().getToggleStrategyName();
+                featureConfiguration = dynamicFeatureConfiguration;
+            }
+            else if (staticFeatureConfiguration.getFeatureToggleStrategyConfig().isPresent()) {
+                FeatureToggleStrategyConfig featureToggleStrategyConfig = staticFeatureConfiguration.getFeatureToggleStrategyConfig().get();
+                toggleStrategyClass = featureToggleStrategyConfig.getToggleStrategyName();
+                featureConfiguration = staticFeatureConfiguration;
+            }
+            if (toggleStrategyClass != null) {
+                FeatureToggleStrategy strategy = featureToggleStrategyFactory.get(toggleStrategyClass);
+                if (strategy != null) {
+                    enabled.set(strategy.check(featureConfiguration));
+                }
+            }
+        }
+    }
+
+    private void checkEnabledFromToggleStrategy(String featureId, Object object, AtomicBoolean enabled)
+    {
+        if (enabled.get()) {
+            FeatureConfiguration dynamicFeatureConfiguration = featureToggleConfiguration.getFeatureConfiguration(featureId);
+            FeatureConfiguration staticFeatureConfiguration = featureMap.get(featureId).getConfiguration();
+            FeatureConfiguration featureConfiguration = null;
+            String toggleStrategyClass = null;
+            if (dynamicFeatureConfiguration != null && dynamicFeatureConfiguration.getFeatureToggleStrategyConfig().isPresent()) {
+                toggleStrategyClass = dynamicFeatureConfiguration.getFeatureToggleStrategyConfig().get().getToggleStrategyName();
+                featureConfiguration = dynamicFeatureConfiguration;
+            }
+            else if (staticFeatureConfiguration.getFeatureToggleStrategyConfig().isPresent()) {
+                FeatureToggleStrategyConfig featureToggleStrategyConfig = staticFeatureConfiguration.getFeatureToggleStrategyConfig().get();
+                toggleStrategyClass = featureToggleStrategyConfig.getToggleStrategyName();
+                featureConfiguration = staticFeatureConfiguration;
+            }
+            if (toggleStrategyClass != null) {
+                FeatureToggleStrategy strategy = featureToggleStrategyFactory.get(toggleStrategyClass);
+                if (strategy != null) {
+                    enabled.set(strategy.check(featureConfiguration, object));
+                }
+            }
+        }
+    }
+
+    private void checkEnabledFromDynamicConfiguration(String featureId, AtomicBoolean enabled)
+    {
+        FeatureConfiguration dynamicFeatureConfiguration = featureToggleConfiguration.getFeatureConfiguration(featureId);
+        if (dynamicFeatureConfiguration != null) {
+            enabled.set(dynamicFeatureConfiguration.isEnabled());
+        }
+    }
+
+    private void checkEnabledFromStaticConfiguration(String featureId, AtomicBoolean enabled)
+    {
+        FeatureConfiguration staticFeatureConfiguration = featureMap.get(featureId).getConfiguration();
+        enabled.set(staticFeatureConfiguration.isEnabled());
+    }
+
+    public Object getCurrentInstance(String featureId)
+    {
+        if (featureToggleConfiguration.getFeatureConfiguration(featureId) != null) {
+            String currentInstance = featureToggleConfiguration.getCurrentInstance(featureId);
+            if (currentInstance != null) {
+                return featureInstanceMap.get(currentInstance);
+            }
+        }
+        Feature<?> feature = featureMap.get(featureId);
+        String defaultInstance = feature.getConfiguration().getDefaultInstance();
+        if (defaultInstance != null) {
+            return featureInstanceMap.get(defaultInstance);
+        }
+        else if (feature.getConfiguration().getFeatureInstances() != null && !feature.getConfiguration().getFeatureInstances().isEmpty()) {
+            return featureInstanceMap.get(feature.getConfiguration().getFeatureInstances().get(0));
+        }
+        else {
+            return null;
+        }
+    }
+
+    public Map<String, Feature<?>> getFeatureMap()
+    {
+        return featureMap;
+    }
+
+    public FeatureToggleConfiguration getFeatureToggleConfiguration()
+    {
+        return featureToggleConfiguration;
+    }
+}
